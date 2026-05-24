c89213df594256313ce058cf53d0b935b9617753
hushell <dom343@gmail.com>
2026-05-24 13:38:23 -0400

    bugfix

--- script/_install.sh
+++ script/_install.sh
@@ -1,15 +1,15 @@
 echo "Installing the necessary packages ..."
-pip install -r script/requirements.txt
+/data/hushell/lingbot-va/lingbot-va/.venv/bin/pip install -r script/requirements.txt
 
 echo "Installing pytorch3d ..."
 # cd third_party/pytorch3d_simplified
-# pip install -e .
+# /data/hushell/lingbot-va/lingbot-va/.venv/bin/pip install -e .
 # cd ../..
-pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable" --no-build-isolation
+/data/hushell/lingbot-va/lingbot-va/.venv/bin/pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable" --no-build-isolation --no-build-isolation
 
 echo "Adjusting code in sapien/wrapper/urdf_loader.py ..."
 # location of sapien, like "~/.conda/envs/RoboTwin/lib/python3.10/site-packages/sapien"
-SAPIEN_LOCATION=$(pip show sapien | grep 'Location' | awk '{print $2}')/sapien
+SAPIEN_LOCATION=$(/data/hushell/lingbot-va/lingbot-va/.venv/bin/pip show sapien | grep 'Location' | awk '{print $2}')/sapien
 # Adjust some code in wrapper/urdf_loader.py
 URDF_LOADER=$SAPIEN_LOCATION/wrapper/urdf_loader.py
 # ----------- before -----------
@@ -35,7 +35,7 @@ sed -i -E 's/("r")(\))( as)/\1, encoding="utf-8") as/g' $URDF_LOADER
 
 echo "Adjusting code in mplib/planner.py ..."
 # location of mplib, like "~/.conda/envs/RoboTwin/lib/python3.10/site-packages/mplib"
-MPLIB_LOCATION=$(pip show mplib | grep 'Location' | awk '{print $2}')/mplib
+MPLIB_LOCATION=$(/data/hushell/lingbot-va/lingbot-va/.venv/bin/pip show mplib | grep 'Location' | awk '{print $2}')/mplib
 
 # Adjust some code in planner.py
 # ----------- before -----------
@@ -49,9 +49,17 @@ sed -i -E 's/(if np.linalg.norm\(delta_twist\) < 1e-4 )(or collide )(or not with
 
 echo "Installing Curobo ..."
 cd envs
-git clone https://github.com/NVlabs/curobo.git
+# RoboTwin requires curobo 0.7.x API (curobo.types.math, curobo.wrap.reacher.motion_gen).
+CUROBO_COMMIT=cca894de9ec74e77a0a4071319c81958991a9108
+if [ ! -d curobo/.git ]; then
+    git clone https://github.com/NVlabs/curobo.git
+fi
 cd curobo
-pip install -e . --no-build-isolation
+git fetch origin "${CUROBO_COMMIT}"
+git checkout "${CUROBO_COMMIT}"
+# Remove stale top-level curobo/ package dir from newer curobo versions (v0.8+).
+rm -rf curobo
+/data/hushell/lingbot-va/lingbot-va/.venv/bin/pip install -e . --no-build-isolation
 cd ../..
 
 echo "Installation basic environment complete!"
